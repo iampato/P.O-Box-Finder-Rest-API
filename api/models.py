41899fbc1e4571142df07cef044024570e1bb225
Iampato <858wpwaweru@gmail.com>
2018-10-24 19:50:30 +0300

    Added maps urls

--- api/models.py
+++ api/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class School(models.Model):
-    title = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     box = models.TextField()
+    mapurl = models.CharField(max_length=1000) 
     created_at = models.DateTimeField(auto_now_add=True)
 
 def __str__(self):
