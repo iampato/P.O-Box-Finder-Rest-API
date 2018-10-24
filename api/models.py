4a28a65df8f756931ecbb3785a42a1ee6bcb614e
Iampato <858wpwaweru@gmail.com>
2018-10-24 20:06:18 +0300

    changed the constraints

--- api/models.py
+++ api/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class School(models.Model):
     name = models.CharField(max_length=200)
-    box = models.TextField()
-    mapurl = models.CharField(max_length=1000) 
+    box = models.CharField(max_length=400)
+    mapurl = models.CharField(max_length=300) 
     created_at = models.DateTimeField(auto_now_add=True)
 
 def __str__(self):
-        return '%s %s' % (self.title, self.body)
+        return self.title
