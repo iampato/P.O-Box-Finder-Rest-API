26b4132c0fcf27a7b84d32fa370b4772e3a06bc9
Iampato <858wpwaweru@gmail.com>
2018-10-24 13:14:08 +0300

    Created database models

--- api/models.py
+++ api/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Note(models.Model):
+class School(models.Model):
     title = models.CharField(max_length=200)
-    body = models.TextField()
+    box = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
 def __str__(self):
